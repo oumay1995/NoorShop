1f0198f75e0844098b6bc96aad46c928e326e9ab
oumay1995 <rezguioumayma95@gmail.com>
2022-09-25 12:58:20 +0100

    ajout login + signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+  {path : "", component : HomeComponent},
+  {path : "login", component : LoginComponent},
+  {path : "signUp", component : SignupComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { FeaturedComponent } from './components/featured/featured.component';
+import { ProductComponent } from './components/product/product.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    HeroComponent,
+    CategoriesComponent,
+    FeaturedComponent,
+    ProductComponent,
+    BlogComponent,
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
